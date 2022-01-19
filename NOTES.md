f198e71bc721fb3baecb949befd25b8098d18adb
Candice Smith MacKenzie <Mahony8@aol.com>
2022-01-19 01:37:44 -0500

    Tuesday 18/22

--- NOTES.md
+++ NOTES.md
@@ -1,5 +1,6 @@
 Shakespearean Translator
 
+
 1. OverView
 Takes everyday language and translates it to Olde English
 
@@ -22,7 +23,19 @@ Randomized insult button(stretch)
 * where to start?
     * homepage
     
+* Image Sources
+Background Image is an inset from the etching called:
+Long View of London from Bankside, a panorama of London by Wenceslaus Hollar, 1647, notable for being rendered all from one viewpoint.
+
+* The Image of Shakespeare on the button:
+The Chandos portrait, held by the National Portrait Gallery, in London. Attributed to John Taylor, is the most famous of the portraits that are believed to depict William Shakespeare (1564–1616). 
+Nobody knows for sure.
 
 
-*Save
-<a class="waves-effect waves-light btn-large">Translate</a>
+* Info about William Shakespeare 
+William Shakespeare (baptized 26 April 1564 – 23 April 1616) was an English playwright, poet and actor, widely regarded as the greatest writer in the English language and the world's greatest dramatist. Born in Stratford-upon-Avon, and sometimes referred to as "The Bard", his works, include 39 plays, 154 sonnets, three narrative poems. (There are other works that may be his, but the debate rages on.) His plays have been translated into every major language and are performed more often than those of any other playwright. He still reaches audiences today due to his unbelievable understanding of human nature and undeniable wit.
+
+Sources- Wikipedia
+Myself
+
+
--- javascripts/index.js
+++ javascripts/index.js
@@ -1,29 +1,41 @@
 
 
-
-// const changeBackgroundImage = () => document.body.style.backgroundImage = "url('theglobe.png')"
-
-
-
 //** Global Variables **//
 let userInputTxt = document.querySelector('#inputTxt');
 const translateButton = document.querySelector('#translateButton');
-//let userOutputTxt = document.querySelector("")
+let userOutputTxt = document.querySelector("#outputTxt");
 const translateUrl = "https://shakespeare1.p.rapidapi.com/shakespeare/translate?text=%3CREQUIRED%3E";
 const insultUrl = "https://shakespeare1.p.rapidapi.com/shakespeare/generate/insult";
 const insultButton = document.querySelector('#shakesBtn');
 
 
 
-//** Templates **/
 
-//** Node Getters **/
+//function getInsult(){
+// fetch("https://shakespeare1.p.rapidapi.com/shakespeare/generate/insult", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "shakespeare1.p.rapidapi.com",
+// 		"x-rapidapi-key": "f82e058679msh72322cf02d18b5fp16c24ajsn798ce02fdf81"
+// 	}
+// })
+// .then(resp => resp.json(){
+// 	console.log(response);
+// })
+// .then(json => {
+   //// userOutputTxt.innerText = json.????
+//})
+// .catch(err => {
+// 	console.error("Shakespeare is too busy to insult you, try again later!");
+// });
+
+//}
 
 
-//** Renderers **/
-
-
-
+//When clicked retrieve insult from API
+insultButton.addEventListener('click', (e)=> {
+    console.log("Hi")
+})
 
 
 //** Code **/
@@ -46,7 +58,6 @@ function handleTranslate(e){
 
 
 translateButton.addEventListener('click', handleTranslate);
-//** On DOMLOAD  **/
 
 // fetch("https://shakespeare1.p.rapidapi.com/shakespeare/translate?text=%3CREQUIRED%3E", {
 // 	"method": "GET",
@@ -62,19 +73,6 @@ translateButton.addEventListener('click', handleTranslate);
 // 	console.error(err);
 // });
 
-// fetch("https://shakespeare1.p.rapidapi.com/shakespeare/generate/insult", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "shakespeare1.p.rapidapi.com",
-// 		"x-rapidapi-key": "f82e058679msh72322cf02d18b5fp16c24ajsn798ce02fdf81"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
 
 
 //`${url}?text=${inputTxt}`
