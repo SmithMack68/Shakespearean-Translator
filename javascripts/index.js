7042d9a605e57fdbe3867d9a281f95d4b16803f2
Candice Smith MacKenzie <Mahony8@aol.com>
2022-01-19 13:33:23 -0500

    fixed first fetch

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,15 +1,17 @@
 
 
 //** Global Variables **//
-let userInputTxt = document.querySelector('#inputTxt');
+let inputTxt = document.querySelector('#inputTxt');
+let outputTxt = document.querySelector("#outputTxt");
 const translateButton = document.querySelector('#translateButton');
-let userOutputTxt = document.querySelector("#outputTxt");
 const translateUrl = "https://shakespeare1.p.rapidapi.com/shakespeare/translate?text=%3CREQUIRED%3E";
 const insultUrl = "https://shakespeare1.p.rapidapi.com/shakespeare/generate/insult";
 const insultButton = document.querySelector('#shakesBtn');
 
 
-
+document.addEventListener('DOMContentLoaded', ()=> {
+    translateButton.addEventListener('click', handleTranslate)
+})
 
 //function getInsult(){
 // fetch("https://shakespeare1.p.rapidapi.com/shakespeare/generate/insult", {
@@ -23,7 +25,7 @@ const insultButton = document.querySelector('#shakesBtn');
 // 	console.log(response);
 // })
 // .then(json => {
-   //// userOutputTxt.innerText = json.????
+   //// outputTxt.innerText = json.????
 //})
 // .catch(err => {
 // 	console.error("Shakespeare is too busy to insult you, try again later!");
@@ -51,28 +53,32 @@ function retrieveTranslation(inputValue){
 }
 
 function handleTranslate(e){
-    let inputValue = userInputTxt.value;
+    let inputValue = inputTxt.value;
 // let translated info = retrieveTranslation(inputValue)
-    fetch()
+  fetch(`https://shakespeare1.p.rapidapi.com/shakespeare/translate?text=${inputValue}`, {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "shakespeare1.p.rapidapi.com",
+		"x-rapidapi-key": "f82e058679msh72322cf02d18b5fp16c24ajsn798ce02fdf81"
+	}
+})
+.then(resp => resp.json())
+.then(data => {
+    debugger
+    //outputTxt.innerText = data.contents
+})
+// .catch(err => {
+// 	console.error(err);
+// });  
 }
 
 
-translateButton.addEventListener('click', handleTranslate);
 
-// fetch("https://shakespeare1.p.rapidapi.com/shakespeare/translate?text=%3CREQUIRED%3E", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "shakespeare1.p.rapidapi.com",
-// 		"x-rapidapi-key": "f82e058679msh72322cf02d18b5fp16c24ajsn798ce02fdf81"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
+// function initialize(){
+//     handleTranslate()
+// }
+// initialize()
 
 
 
-//`${url}?text=${inputTxt}`
+// `${url}?text=${inputTxt}`
