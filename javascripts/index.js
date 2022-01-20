26d65b815612b0ee24877ecf83d4a7e4f51390ec
Candice Smith MacKenzie <Mahony8@aol.com>
2022-01-20 03:14:31 -0500

    insult fetch

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,6 +1,7 @@
 //** Global Variables **//
 let inputTxt = document.querySelector('#inputTxt');
 let outputTxt = document.querySelector("#outputTxt");
+let returnedInsult = document.querySelector('.modal-content')
 const translateButton = document.querySelector('#translateButton');
 const translateUrl = "https://shakespeare1.p.rapidapi.com/shakespeare/translate?text=%3CREQUIRED%3E";
 const insultUrl = "https://shakespeare1.p.rapidapi.com/shakespeare/generate/insult";
@@ -9,29 +10,26 @@ const insultButton = document.querySelector('#shakesBtn');
 
 document.addEventListener('DOMContentLoaded', () => {
     translateButton.addEventListener('click', handleTranslate)
+    insultButton.addEventListener('click', getInsult)
 })
 
-//When clicked retrieve insult from API
-insultButton.addEventListener('click', (e)=> {
-    console.log("Hi")
-})
 
-//function getInsult(){
-    // fetch("https://shakespeare1.p.rapidapi.com/shakespeare/generate/insult", {
-    // 	"method": "GET",
-    // 	"headers": {
-    // 		"x-rapidapi-host": "shakespeare1.p.rapidapi.com",
-    // 		"x-rapidapi-key": "f82e058679msh72322cf02d18b5fp16c24ajsn798ce02fdf81"
-    // 	}
-    // })
-    // .then(resp => resp.json())
-    // .then(data => {
-       //// outputTxt.innerText = json.????
-    //})
-    // .catch(() => alert("Shakespeare is too busy to insult you, try again later!");
-    // });
-    
-    //}
+function getInsult(e){
+    fetch("https://shakespeare1.p.rapidapi.com/shakespeare/generate/insult", {
+    	"method": "GET",
+    	"headers": {
+    		"x-rapidapi-host": "shakespeare1.p.rapidapi.com",
+    		"x-rapidapi-key": "f82e058679msh72322cf02d18b5fp16c24ajsn798ce02fdf81"
+    	}
+    })
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        //data.contents.taunts
+    })
+    // .catch(() => alert("Shakespeare is too busy to insult you, try again later!")
+    // );
+   }
     
 function handleTranslate(e){
     let inputValue = inputTxt.value;
@@ -44,10 +42,11 @@ function handleTranslate(e){
 })
 .then(resp => resp.json())
 .then(data => {
-    //console.log(data.contents.translated)
-    //debugger
     const outputValue = data.contents.translated
     outputTxt.value = outputValue
 })
- .catch(() => alert("error message")) 
+ .catch(() => alert("Please make sure to enter your text")) 
 }
+// function eraseText(){
+  //document.getElementById("inputTxt").value = ""
+//}
