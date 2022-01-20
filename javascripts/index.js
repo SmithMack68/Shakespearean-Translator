4b083f0b4ff6f222c36eee379266201412284039
Candice Smith MacKenzie <Mahony8@aol.com>
2022-01-19 21:48:49 -0500

    added modal

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,5 +1,3 @@
-
-
 //** Global Variables **//
 let inputTxt = document.querySelector('#inputTxt');
 let outputTxt = document.querySelector("#outputTxt");
@@ -9,52 +7,34 @@ const insultUrl = "https://shakespeare1.p.rapidapi.com/shakespeare/generate/insu
 const insultButton = document.querySelector('#shakesBtn');
 
 
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', () => {
     translateButton.addEventListener('click', handleTranslate)
 })
-
-//function getInsult(){
-// fetch("https://shakespeare1.p.rapidapi.com/shakespeare/generate/insult", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "shakespeare1.p.rapidapi.com",
-// 		"x-rapidapi-key": "f82e058679msh72322cf02d18b5fp16c24ajsn798ce02fdf81"
-// 	}
-// })
-// .then(resp => resp.json(){
-// 	console.log(response);
-// })
-// .then(json => {
-   //// outputTxt.innerText = json.????
-//})
-// .catch(err => {
-// 	console.error("Shakespeare is too busy to insult you, try again later!");
-// });
-
-//}
-
 
 //When clicked retrieve insult from API
 insultButton.addEventListener('click', (e)=> {
     console.log("Hi")
 })
 
-
-//** Code **/
-// document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.collapsible');
-//     var instances = M.Collapsible.init(elems, options);
-//   });
-
-
-function retrieveTranslation(inputValue){
-
-    return translatedData
-}
-
+//function getInsult(){
+    // fetch("https://shakespeare1.p.rapidapi.com/shakespeare/generate/insult", {
+    // 	"method": "GET",
+    // 	"headers": {
+    // 		"x-rapidapi-host": "shakespeare1.p.rapidapi.com",
+    // 		"x-rapidapi-key": "f82e058679msh72322cf02d18b5fp16c24ajsn798ce02fdf81"
+    // 	}
+    // })
+    // .then(resp => resp.json())
+    // .then(data => {
+       //// outputTxt.innerText = json.????
+    //})
+    // .catch(() => alert("Shakespeare is too busy to insult you, try again later!");
+    // });
+    
+    //}
+    
 function handleTranslate(e){
     let inputValue = inputTxt.value;
-// let translated info = retrieveTranslation(inputValue)
   fetch(`https://shakespeare1.p.rapidapi.com/shakespeare/translate?text=${inputValue}`, {
 	"method": "GET",
 	"headers": {
@@ -64,21 +44,10 @@ function handleTranslate(e){
 })
 .then(resp => resp.json())
 .then(data => {
-    debugger
-    //outputTxt.innerText = data.contents
+    //console.log(data.contents.translated)
+    //debugger
+    const outputValue = data.contents.translated
+    outputTxt.value = outputValue
 })
-// .catch(err => {
-// 	console.error(err);
-// });  
+ .catch(() => alert("error message")) 
 }
-
-
-
-// function initialize(){
-//     handleTranslate()
-// }
-// initialize()
-
-
-
-// `${url}?text=${inputTxt}`
